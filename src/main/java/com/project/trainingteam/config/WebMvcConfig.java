90a10b065160ab00db203f42f842d9101bf2d8c6
Huynh Chanh <chanhhuynhst7@gmail.com>
2023-07-11 11:15:48 +0700

    config CORS

--- src/main/java/com/project/trainingteam/config/WebMvcConfig.java
+++ src/main/java/com/project/trainingteam/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.project.trainingteam.config;
 import com.project.trainingteam.config.security.AccessControlInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
-//    @Autowired
+    //    @Autowired
 //    private AccessControlInterceptor accessControlInterceptor;
 //
 //    @Override
@@ -24,4 +25,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/v3/api-docs/**")
 //                .excludePathPatterns("/v3/api-docs.yaml");
 //    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
 }
